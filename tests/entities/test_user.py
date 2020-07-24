0e925e390e7dfaa117bef3d86d96880f4b771ec5
alex13th <alex13th76@gmail.com>
2020-07-24 12:36:44 +0800

    Released class User (#2)
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   tests/entities/test_user.py
    	modified:   wakebot/entities/user.py

--- tests/entities/test_user.py
+++ tests/entities/test_user.py
@@ -13,16 +13,34 @@ class UserTestCase(unittest.TestCase):
 
         self.assertEqual(user.firstname, "Firstname")
         self.assertEqual(user.lastname, "Lastname")
-        self.assertEqual(user.middlename, "Midlename")
+        self.assertEqual(user.middlename, "Middlename")
         self.assertEqual(user.displayname, "Displayname")
         self.assertEqual(user.phone_number, "+7999555443322")
         self.assertEqual(user.telegram_id, 9876)
         self.assertEqual(user.user_id, 3456)
 
+    def test_set_properties(self):
+        user = User("Firstname")
+        user.firstname = "Newname"
+        user.lastname = "Lastname1"
+        user.middlename = "Middlename1"
+        user.displayname = "Displayname1"
+        user.phone_number = "+7999555443311"
+        user.telegram_id = 1234
+        user.user_id = 9876
+
+        self.assertEqual(user.firstname, "Newname")
+        self.assertEqual(user.lastname, "Lastname1")
+        self.assertEqual(user.middlename, "Middlename1")
+        self.assertEqual(user.displayname, "Displayname1")
+        self.assertEqual(user.phone_number, "+7999555443311")
+        self.assertEqual(user.telegram_id, 1234)
+        self.assertEqual(user.user_id, 9876)
+
     def test_diplay_name_full(self):
         """Формирование отображаемого имени с отчеством"""
         user = User("Firstname", "Lastname", "Middlename",
-                    "+7999555443322", 9876, 3456)
+                    None, "+7999555443322", 9876, 3456)
         self.assertEqual(user.displayname, "Lastname Firstname Middlename")
 
     def test_diplay_name_without_middlename(self):
@@ -39,14 +57,14 @@ class UserTestCase(unittest.TestCase):
         """Формирование отображаемого имени только из имени"""
         """Формирование отображаемого имени без фамилии"""
         user = User("Firstname")
-        self.assertEqual(user.displayname, "Firstname Middlename")
+        self.assertEqual(user.displayname, "Firstname")
 
     def test_to_str(self):
         """Строка"""
         user = User("Firstname", "Lastname", "Middlename",
-                    "Displayname", "+7999555443322", 9876, 3456)
+                    None, "+7999555443322", 9876, 3456)
 
-        self.assertEqual(str(user), "Lastname Firstname Middlename (tg:9876)")
+        self.assertEqual(str(user), "Lastname Firstname Middlename")
 
 
 try:
--- wakebot/entities/user.py
+++ wakebot/entities/user.py
@@ -5,46 +5,34 @@ class User:
     Класс модели пользователя
     """
 
-    def __init__(self, 
+    def __init__(self,
                  firstname, lastname=None, middlename=None, displayname=None,
                  phone_number=None, telegram_id=None, user_id=None):
-        pass
-
-    @property
-    def user_id(self):
-        """ Идентификатор пользователя """
-        pass
-
-    @property
-    def telegram_id(self):
-        """ Идентификатор в Телеграм """
-        pass
-
-    @property
-    def firstname(self):
-        """ Имя """
-        pass
-
-    @property
-    def lastname(self):
-        """ Фамилия """
-        pass
-
-    @property
-    def middlename(self):
-        """ Отчество """
-        pass
+        self.firstname = firstname
+        self.lastname = lastname
+        self.middlename = middlename
+        self._displayname = displayname
+        self.phone_number = phone_number
+        self.telegram_id = telegram_id
+        self.user_id = user_id
 
     @property
     def displayname(self):
         """ Отображаемое имя """
-        pass
+        if self._displayname:
+            return self._displayname
 
-    @property
-    def phone_number(self):
-        """ Номер телефона """
-        pass
+        result = self.lastname or ""
+        result += " " + self.firstname + " "
+        result += self.middlename or ""
+
+        return result.strip()
+
+    @displayname.setter
+    def displayname(self, value):
+        """ Отображаемое имя """
+        self._displayname = value
 
     def __str__(self):
         """ Строковое представление """
-        return ""
+        return self.displayname
