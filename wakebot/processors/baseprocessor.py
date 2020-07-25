d88bf03bc02f9b83e5ec3045fc7173261c0e2604
alex13th <alex13th76@gmail.com>
2020-07-25 11:03:59 +0800

    Added a base processor class model (#16).
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   wakebot/processors/__init__.py
    	new file:   wakebot/processors/baseprocessor.py

---
+++ wakebot/processors/baseprocessor.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+class BaseProcessor:
+    """ Базовый класс обработки сообщений
+    """
+
+    def __init__(self, provider, state):
+        """
+            provider: объект основного процессора бота;
+            state: объект состояния чата.
+        """
+        pass
+
+    @property
+    def provided_commands(self):
+        """ Кортеж поддерживаемых команд """
+        pass
+
+    """
+    Методы реализуемые потомками по мере необходимости
+
+    def proceed_text(self, text):
+        pass
+
+    def proceed_command(self, command):
+        pass
+
+    def proceed_callback_query(self, callback):
+        pass
+
+    def proceed_contact(self, proceed_contact):
+        pass
+    """
