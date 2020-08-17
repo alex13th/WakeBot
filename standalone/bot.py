72498f0bf976b6c68696a1251d9887741427b6a4
Aleksei Sukharev <alex13th76@gmail.com>
2020-08-17 11:36:51 +0800

    Improved a standalone bot to use the enviroment variables.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   .gitignore
    	modified:   standalone/bot.py

--- standalone/bot.py
+++ standalone/bot.py
@@ -1,4 +1,5 @@
-import sqlite3
+import psycopg2
+import os
 
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
@@ -10,8 +11,11 @@ from wakebot.processors.default import DefaultProcessor
 from wakebot.processors.wake import WakeProcessor
 from wakebot.adapters.data import MemoryDataAdapter
 from wakebot.adapters.state import StateManager
-from wakebot.adapters.sqlite.wake import SqliteWakeAdapter
-from config import TOKEN
+from wakebot.adapters.postgres.wake import PostgressWakeAdapter
+# from config import TOKEN
+
+TOKEN = os.environ["TOKEN"]
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -21,8 +25,9 @@ dp.middleware.setup(LoggingMiddleware())
 state_manager = StateManager(MemoryDataAdapter())
 
 default_processor = DefaultProcessor(dp, RuGeneral)
-connection = sqlite3.connect("standalone/wake.db")
-wake_adapter = SqliteWakeAdapter(connection)
+connection = psycopg2.connect(DATABASE_URL)
+
+wake_adapter = PostgressWakeAdapter(connection)
 wake_processor = WakeProcessor(dp, state_manager, RuGeneral, wake_adapter)
 
 if __name__ == "__main__":
